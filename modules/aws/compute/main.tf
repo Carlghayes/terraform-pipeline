d369c20668ca688629fb983baacec0c7899c2159
Carl Hayes <carlhayes@Carls-MacBook-Pro.local>
2026-05-10 16:24:16 -0500

    updated location of cloud-init template

--- modules/aws/compute/main.tf
+++ modules/aws/compute/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "this" {
   instance_type          = var.instance_type
   key_name               = aws_key_pair.this.key_name
   vpc_security_group_ids = [var.security_group_id]
-  user_data              = templatefile("${path.module}/../../cloud-init.yml.tpl", {
+  user_data              = templatefile("${path.module}/cloud-init.yml.tpl", {
     ssh_public_key = var.ssh_public_key
     ingress_rules  = var.ingress_rules
   })
